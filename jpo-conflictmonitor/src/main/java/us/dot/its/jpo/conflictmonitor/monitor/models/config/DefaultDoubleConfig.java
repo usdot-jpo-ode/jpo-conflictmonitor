0ed0d2ef3cbf4f327b1902756f85bb98f645b5f9
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2023-03-07 18:36:50 -0700

    Remove unused classes

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models/config/DefaultDoubleConfig.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models/config/DefaultDoubleConfig.java
@@ -1,6 +1,6 @@
 package us.dot.its.jpo.conflictmonitor.monitor.models.config;
 
-import lombok.AllArgsConstructor;
+
 
 
 public class DefaultDoubleConfig extends DefaultConfig<Double> {}
--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models/config/IntersectionIntConfig.java
+++
@@ -1,5 +0,0 @@
-package us.dot.its.jpo.conflictmonitor.monitor.models.config;
-
-public class IntersectionIntConfig extends IntersectionConfig<String> {
-    
-}
