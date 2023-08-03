e258cf813241a4b27a30fea33de1c6a1eae296c6
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2023-08-02 19:05:37 -0600

    Spat timing statistics

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/analytics/StopLineStopAnalytics.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/analytics/StopLineStopAnalytics.java
@@ -1,6 +1,7 @@
 package us.dot.its.jpo.conflictmonitor.monitor.analytics;
 
 import static us.dot.its.jpo.conflictmonitor.monitor.algorithms.stop_line_stop.StopLineStopConstants.*;
+import static us.dot.its.jpo.conflictmonitor.monitor.utils.SpatUtils.getSignalGroupState;
 
 import org.locationtech.jts.geom.CoordinateXY;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import us.dot.its.jpo.conflictmonitor.monitor.models.events.StopLineStopEvent;
 import us.dot.its.jpo.conflictmonitor.monitor.models.spat.SpatAggregator;
 import us.dot.its.jpo.conflictmonitor.monitor.models.spat.SpatTimestampExtractor;
 import us.dot.its.jpo.conflictmonitor.monitor.utils.BsmUtils;
+import us.dot.its.jpo.conflictmonitor.monitor.utils.SpatUtils;
 import us.dot.its.jpo.geojsonconverter.pojos.spat.MovementState;
 import us.dot.its.jpo.geojsonconverter.pojos.spat.ProcessedSpat;
 import us.dot.its.jpo.ode.model.OdeBsmData;
@@ -26,6 +28,7 @@ import us.dot.its.jpo.ode.plugin.j2735.J2735MovementPhaseState;
 import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 @Component(DEFAULT_SIGNAL_STATE_VEHICLE_STOPS_ALGORITHM)
@@ -57,7 +60,9 @@ public class StopLineStopAnalytics implements StopLineStopAlgorithm {
         OdeBsmData lastStoppedBsm = stoppedBsmList.get(stoppedBsmList.size() - 1);
 
         long firstTimestamp = BsmTimestampExtractor.getBsmTimestamp(firstStoppedBsm);
+        logger.info("First stopped BSM timestamp {}", firstTimestamp);
         long lastTimestamp = BsmTimestampExtractor.getBsmTimestamp(lastStoppedBsm);
+        logger.info("Last stopped BSM timestamp {}", lastTimestamp);
         long durationStoppedMillis = Math.abs(lastTimestamp - firstTimestamp);
         logger.info("Stop duration {} ms", durationStoppedMillis);
         double minTimeStoppedSeconds = parameters.getMinTimeStopped();
@@ -70,12 +75,13 @@ public class StopLineStopAnalytics implements StopLineStopAlgorithm {
         ProcessedSpat firstSpat = spats.getSpatAtTime(firstTimestamp);
         ProcessedSpat lastSpat = spats.getSpatAtTime(lastTimestamp);
 
-        var spatStatistics = spats.getSpatStatistics();
+
 
         StopLineStopEvent event = new StopLineStopEvent();
 
         event.setInitialTimestamp(firstTimestamp);
         event.setFinalTimestamp(lastTimestamp);
+
         event.setIntersectionID(path.getIntersection().getIntersectionId());
         event.setRoadRegulatorID(path.getIntersection().getRoadRegulatorId());
         event.setIngressLane(ingressLane.getId());
@@ -90,26 +96,44 @@ public class StopLineStopAnalytics implements StopLineStopAlgorithm {
         event.setLongitude(firstBsmPosition.getX());
         event.setLatitude(firstBsmPosition.getY());
 
-
-        LaneConnection connection = path.getIntersection().getLaneConnection(ingressLane, egressLane);
-        if (connection != null) {
-            event.setSignalGroup(connection.getSignalGroup());
-            J2735MovementPhaseState firstSignalState = getSignalGroupState(firstSpat, connection.getSignalGroup());
-            J2735MovementPhaseState lastSignalState = getSignalGroupState(lastSpat, connection.getSignalGroup());
-            event.setInitialEventState(firstSignalState);
-            event.setFinalEventState(lastSignalState);
+        int signalGroup = -1;
+        if (egressLane != null) {
+            LaneConnection connection = path.getIntersection().getLaneConnection(ingressLane, egressLane);
+            if (connection != null) {
+                signalGroup = connection.getSignalGroup();
+            } else {
+                logger.info("No lane connection found for ingressLane {} and egressLane {}", ingressLane, egressLane);
+            }
+        } else {
+            Set<Integer> signalGroups = path.getIntersection().getSignalGroupsForIngressLane(ingressLane);
+            if (signalGroups.size() == 1) {
+                signalGroup = signalGroups.iterator().next();
+            } else {
+                logger.info("No egress lane found for path {}, and ingress lane {} has multiple signal groups {}, can't determine signalGroup to generate StopLinePassage event", path, ingressLane, signalGroups);
+                return null;
+            }
         }
 
+        if (signalGroup > -1) {
+            event.setSignalGroup(signalGroup);
+            J2735MovementPhaseState firstSignalState = getSignalGroupState(firstSpat, signalGroup);
+            J2735MovementPhaseState lastSignalState = getSignalGroupState(lastSpat, signalGroup);
+            event.setInitialEventState(firstSignalState);
+            event.setFinalEventState(lastSignalState);
+
+            List<ProcessedSpat> filteredSpats = SpatUtils.filterSpatsByTimestamp(spats.getSpats(), firstTimestamp, lastTimestamp);
+            String spatDesc = SpatUtils.describeSpats(filteredSpats, signalGroup);
+            logger.info(spatDesc);
+            SpatUtils.SpatStatistics spatStatistics = SpatUtils.getSpatStatistics(filteredSpats, signalGroup);
+
+            event.setTimeStoppedDuringRed(spatStatistics.getTimeStoppedDuringRed());
+            event.setTimeStoppedDuringYellow(spatStatistics.getTimeStoppedDuringYellow());
+            event.setTimeStoppedDuringGreen(spatStatistics.getTimeStoppedDuringGreen());
+        }
+
+        logger.info("StopLineStopEvent: {}", event);
         return event;
     }
 
-    public J2735MovementPhaseState getSignalGroupState(ProcessedSpat spat, int signalGroup){
-        long spatTime = SpatTimestampExtractor.getSpatTimestamp(spat);
-        for(MovementState state: spat.getStates()){
-            if(state.getSignalGroup() == signalGroup && state.getStateTimeSpeed().size() > 0){
-                return state.getStateTimeSpeed().get(0).getEventState();
-            }
-        }
-        return null;
-    }
+
 }
---
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/utils/SpatUtils.java
@@ -0,0 +1,155 @@
+package us.dot.its.jpo.conflictmonitor.monitor.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import us.dot.its.jpo.conflictmonitor.monitor.models.spat.SpatTimestampExtractor;
+import us.dot.its.jpo.geojsonconverter.pojos.spat.MovementEvent;
+import us.dot.its.jpo.geojsonconverter.pojos.spat.MovementState;
+import us.dot.its.jpo.geojsonconverter.pojos.spat.ProcessedSpat;
+import us.dot.its.jpo.ode.plugin.j2735.J2735MovementPhaseState;
+
+import java.util.*;
+
+public class SpatUtils {
+
+    public static J2735MovementPhaseState getSignalGroupState(ProcessedSpat spat, int signalGroup){
+        for(MovementState state: spat.getStates()){
+            if(state.getSignalGroup() == signalGroup && state.getStateTimeSpeed().size() > 0){
+                return state.getStateTimeSpeed().get(0).getEventState();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Filter SPATs between two timestamps.
+     * @param spats - List of SPATs, assumed to be ordered by timestamp
+     * @param startTimestamp - start timestamp, inclusive
+     * @param endTimestamp - end timestamp, inclusive
+     * @return List of SPATs between startTimestamp and endTimestamp, inclusive
+     */
+    public static List<ProcessedSpat> filterSpatsByTimestamp(List<ProcessedSpat> spats, long startTimestamp, long endTimestamp) {
+        List<ProcessedSpat> filteredSpats = new ArrayList<>();
+        for (ProcessedSpat spat : spats) {
+            long spatTimestamp = SpatTimestampExtractor.getSpatTimestamp(spat);
+            if (spatTimestamp >= startTimestamp && spatTimestamp <= endTimestamp) {
+                filteredSpats.add(spat);
+            }
+        }
+        return filteredSpats;
+    }
+
+    public static String describeSpats(List<ProcessedSpat> spats, int signalGroupId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SPATs for signal group ");
+        sb.append(signalGroupId);
+        sb.append(":\n");
+        List<SpatTiming> spatTimingList = getSpatTiming(spats, signalGroupId);
+        for (SpatTiming spatTiming : spatTimingList) {
+            sb.append(spatTiming.getTimestamp());
+            sb.append(" (Δ ");
+            sb.append(spatTiming.getDuration());
+            sb.append("): ");
+            sb.append(spatTiming.getState());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static List<SpatTiming> getSpatTiming(List<ProcessedSpat> spats, int signalGroupId) {
+        var timingList = new ArrayList<SpatTiming>();
+
+        for (ProcessedSpat spat : spats) {
+            long timestamp = SpatTimestampExtractor.getSpatTimestamp(spat);
+            J2735MovementPhaseState state = getSignalGroupState(spat, signalGroupId);
+            var spatTiming = new SpatTiming(timestamp, state);
+            timingList.add(spatTiming);
+        }
+
+        // Estimate duration of each state based on the time between SPATs
+        ListIterator<SpatTiming> iterator = timingList.listIterator();
+        while (iterator.hasNext()) {
+            var spatTiming = iterator.next();
+            int compareIndex = 0;
+            if (iterator.hasNext()) {
+                compareIndex = iterator.nextIndex();
+            } else if (iterator.hasPrevious()) {
+                compareIndex = iterator.previousIndex();
+            }
+            var compareTiming = timingList.get(compareIndex);
+            long duration = Math.abs(compareTiming.getTimestamp() - spatTiming.getTimestamp());
+            spatTiming.setDuration(duration);
+        }
+
+        return timingList;
+    }
+
+    /**
+     * Get statistics for the amount of time during each signal phase while
+     * @return {@link SpatStatistics}
+     */
+    public static SpatStatistics getSpatStatistics(List<ProcessedSpat> spats, int signalGroupId) {
+        long redMillis = 0;
+        long yellowMillis = 0;
+        long greenMillis = 0;
+        List<SpatTiming> spatTimingList = getSpatTiming(spats, signalGroupId);
+        for (SpatTiming spatTiming : spatTimingList) {
+        switch (spatTiming.getState()) {
+                case STOP_AND_REMAIN:
+                    redMillis += spatTiming.getDuration();
+                    break;
+                case PERMISSIVE_CLEARANCE:
+                case PROTECTED_CLEARANCE:
+                    yellowMillis += spatTiming.getDuration();
+                    break;
+                case PERMISSIVE_MOVEMENT_ALLOWED:
+                case PROTECTED_MOVEMENT_ALLOWED:
+                    greenMillis += spatTiming.getDuration();
+                    break;
+                default:
+                    break;
+            }
+        }
+        return new SpatStatistics(redMillis/1000.0, yellowMillis/1000.0, greenMillis/1000.0);
+    }
+
+    @Getter
+    @Setter
+    public static class SpatTiming {
+        public SpatTiming(long timestamp, J2735MovementPhaseState state) {
+            this.timestamp = timestamp;
+            this.state = state;
+        }
+        private long timestamp;
+        private J2735MovementPhaseState state;
+        private long duration;
+    }
+
+
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @ToString
+    public static class SpatStatistics {
+        /**
+         * The amount of time (seconds) the event state was ‘stop-and-remain’
+         * while the vehicle was stopped.
+         */
+        private double timeStoppedDuringRed;
+
+        /**
+         * The amount of time (seconds) the event state was ‘protected-clearance’
+         * or ‘permissive-clearance’ while the vehicle was stopped.
+         */
+        private double timeStoppedDuringYellow;
+
+        /**
+         * The amount of time (seconds) the event state was ‘protected-movement-allowed’ or ‘permissive-movement-allowed’ while the
+         * vehicle was stopped.
+         */
+        private double timeStoppedDuringGreen;
+    }
+}
