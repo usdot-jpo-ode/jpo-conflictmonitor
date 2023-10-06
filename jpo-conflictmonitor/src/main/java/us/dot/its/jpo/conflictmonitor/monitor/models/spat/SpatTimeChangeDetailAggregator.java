3a8043a9f3b8a402edb872ed6b2c8667e5ec79fd
John-Wiens <jwiens3141@gmail.com>
2023-10-06 08:37:01 -0600

    Adding Check to prevent mismatched processing

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models/spat/SpatTimeChangeDetailAggregator.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/models/spat/SpatTimeChangeDetailAggregator.java
@@ -19,7 +19,7 @@ public class SpatTimeChangeDetailAggregator {
 
     @Getter
     @Setter
-    private int messageBufferSize = 2;
+    private int messageBufferSize = 100;
 
 
     // Parameterless constructor needed by Jackson
@@ -70,7 +70,14 @@ public class SpatTimeChangeDetailAggregator {
                 this.spatTimeChangeDetails.get(1)
             );
             this.spatTimeChangeDetails.remove(0);
-            return outputPair;
+            // logger.info("Emitting Spat Pair First {}  {}  Second {}   {}" + outputPair.getFirst().getTimestamp(),outputPair.getFirst().getIntersectionID(), outputPair.getSecond().getTimestamp(),outputPair.getSecond().getOriginIP());
+            long delta = Math.abs(outputPair.getFirst().getTimestamp() - outputPair.getSecond().getTimestamp());
+            if(delta < 150){
+                //logger.info("Emitting Spat Pair First {}  {}  Second {}   {}", outputPair.getFirst().getTimestamp(),outputPair.getFirst().getIntersectionID(), outputPair.getSecond().getTimestamp(), outputPair.getSecond().getIntersectionID());
+                return outputPair;
+            }else{
+                return null;
+            }
         }
         else{
             return null;
