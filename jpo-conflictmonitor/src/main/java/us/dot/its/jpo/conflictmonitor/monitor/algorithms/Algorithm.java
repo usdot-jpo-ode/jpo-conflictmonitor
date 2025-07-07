2e505c8425c68b032ccbc34b206298ab8b736ba6
john-wiens <jwiens3141@gmail.com>
2025-07-07 15:28:40 -0600

    Adding additional javadocs to reduce number of warnings

--- jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/Algorithm.java
+++ jpo-conflictmonitor/src/main/java/us/dot/its/jpo/conflictmonitor/monitor/algorithms/Algorithm.java
@@ -3,6 +3,6 @@ package us.dot.its.jpo.conflictmonitor.monitor.algorithms;
 
 /**
  * General interface for an algorithm that can be configured with parameters, and started and stopped.
- * 
+ * @param <TParameters> the type of parameters used to configure the algorithm
  */
 public interface Algorithm<TParameters> extends ExecutableAlgorithm, ConfigurableAlgorithm<TParameters>  { }
